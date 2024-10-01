bd3e0ce0e902cb9d5fdbea60771d0612106cb61a
haley227 <haleysmith227@gmail.com>
2024-09-30 21:46:32 -0700

    Add three js modules

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,25 @@
-import { ThreeDHero } from "../components/ThreeDHero/ThreeDHero";
+import About from "../components/About";
+import { StarsCanvas } from "../components/canvas";
+import Experience from "../components/Experience";
+import Feedbacks from "../components/Feedbacks";
+import Tech from "../components/Tech";
+import Hero from "../components/ThreeDHero/ThreeDHero";
+import Works from "../components/Works";
 
 export default function Home() {
   return (
-    <ThreeDHero />
+    <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <StarsCanvas />
+        </div>
+      </div>
   );
 }
